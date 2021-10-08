7c07fd61bd1151adb259fade889a240fb6463b3e
PowerofAvenger <88158323+PowerofAvenger@users.noreply.github.com>
2021-10-08 16:14:56 +0530

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -1,22 +1,22 @@
 #kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
-#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#make by https://t.me/godkiller_mark2_sinx for chutiapa 🤔😑 you dont kang this 
 #okay to ab isme cheeze bharte h 
 #if you read only this then okay else
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "X-USERBOT"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+BOT =  = str(NAME) if NAME else "X-USERBOT"
 #op for keinshin
 
-Keinshin = "[KEINSHIN](https://t.me/keinshin)"
-OP = "[BLACK LIGHTNING](https://github.com/KeinShin/Black-Lightning)"
-OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+Keinshin = "[MAKER](https://t.me/godkiller_mark2_sinx)"
+OP = "[BLACK LIGHTNING](https://github.com/powerofavenger/sinneruserbot)"
+OKAY = "[SUPPORT GROUP](https://t.me/daisy_johnson_support)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "BLACK LIGHTNING BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
-USERBOT = " HELLO MASTER MY NAME IS BLACK LIGHTNING BOT I AM A BEST USERBOT 💝"
+ALIVE = "X-USERBOT BOT IS ON 🔥 FIRE 🔥" #make by godkiller_mark2 for black lightning
+USERBOT = " HELLO MASTER MY NAME IS X-USERBOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
